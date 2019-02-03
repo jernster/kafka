102d8f63e95605606395e1ec181f2a2531a8ba99
Jon Ernster <jon.ernster@gmail.com>
2019-02-02 21:47:18 -0700

    Cleanup: Finalized

--- connect/connector-status.py
+++ connect/connector-status.py
@@ -2,7 +2,7 @@ import argparse
 import requests
 
 parser = argparse.ArgumentParser(description="Kafka Connector statuses")
-parser.add_argument("--url", help="Kafka Connect URL", required=True) # http://localhost:8083/connectors/ 
+parser.add_argument("--url", help="Kafka Connect URL", required=True) # http://localhost:8083/connectors 
 
 args = parser.parse_args()
 
@@ -10,33 +10,16 @@ connect_url = args.url
 
 r = requests.get(connect_url)
 
-# print(r) # <Response [200]>
-
-#print(type(r.json))
 connectors = []
 connectors = r.json()
-#print(type(connectors))
-#print(r.json())
-#print(connectors)
 
 for connector in connectors:
-    #print(connector)
     connect_status_url = args.url + '/' + connector + '/status'
-    #print(connect_status_url)
+    
     r = requests.get(connect_status_url)
-    connector_status = r.json()
-    #print(type(connector_status))
-    #print(connector_status)
-    for k,v in connector_status.items():
-        #str = "{0}: {1}".format(k,v)
-        #print(str)
-        #if type(v) == dict:
-            #print(k)
-        #    for k,v in v.items():
-        #        print(f"{k}: {v}")
-            #print("{0} is a dict".format(v))
-            #print(v.items())
 
-        ##print(type(v))
-        print(f"{k}: {v}")
+    connector_status = r.json()
+
+    for k,v in connector_status.items():
+        print("{0}: {1}".format(k,v))
     print('=' * 50)
