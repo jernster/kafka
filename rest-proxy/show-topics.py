43a60ab7c822c5a0cffb9537fdbd20bd8d84a276
Jon Ernster <jon.ernster@gmail.com>
2019-03-29 20:52:02 -0600

    org structure change

---
+++ rest-proxy/show-topics.py
@@ -0,0 +1,14 @@
+import argparse
+import requests
+
+parser = argparse.ArgumentParser(description="Display topics from REST Proxy")
+parser.add_argument("--url", help="Provide URL to REST Proxy", required=True) # http://localhost:8082/topics/ 
+
+args = parser.parse_args()
+
+url = args.url
+
+r = requests.get(url)
+
+for topic in r.json():
+    print(topic)
