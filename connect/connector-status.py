115ab7322aaff4508f45ca18587e1eeaa2bd0419
Jon Ernster <jon@elopers.dev>
2019-04-16 21:16:42 -0600

    Refactored to be way better

--- connect/connector-status.py
+++ connect/connector-status.py
@@ -1,25 +1,31 @@
 import argparse
 import requests
 
-parser = argparse.ArgumentParser(description="Kafka Connector statuses")
-parser.add_argument("--url", help="Kafka Connect URL (http://localhost:8083/connectors)", required=True) # http://localhost:8083/connectors 
+parser = argparse.ArgumentParser(description="Kafka Connector status")
+parser.add_argument("--url", "-u", help="Kafka Connect URL (http://localhost:8083/connectors/)", required=True) # http://localhost:8083/connectors/ 
+parser.add_argument("--connector", "-c", help="Connector name", required=True) 
 
 args = parser.parse_args()
 
 connect_url = args.url
+connector_name = args.connector
 
-r = requests.get(connect_url)
+connect_status_url = args.url + connector_name + '/status'
+   
+r = requests.get(connect_status_url)
 
-connectors = []
-connectors = r.json()
+connector_status = r.json()
 
-for connector in connectors:
-    connect_status_url = args.url + '/' + connector + '/status'
-    
-    r = requests.get(connect_status_url)
+print("Connector Name: {0}".format(connector_status['name']))
+print('=' * 50)
 
-    connector_status = r.json()
+for k,v in connector_status['connector'].items():
+    print("{0}: {1}".format(k,v), end=' ')
+print()
+print('=' * 50)
 
-    for k,v in connector_status.items():
-        print("{0}: {1}".format(k,v))
-    print('=' * 50)
+for c in connector_status['tasks']:
+    print("Task: ", end=' ')
+    print("state: {0}".format(c['state']), end=' ')
+    print("id: {0}".format(c['id']), end=' ')
+    print("worker_id: {0}".format(c['worker_id']), end='\n')
